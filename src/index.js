7e7fb231748bf8bf6fd14f93c5f2da63403607a8
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-01-29 10:28:53 +0200

    fox1

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { App } from 'components/App';
-// import { Basics } from 'Basics';
+
 const theme = {
   colors: {
     black: '#212121',
@@ -23,8 +23,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <App />
-
-      {/* <Basics /> */}
     </ThemeProvider>
   </React.StrictMode>
 );
